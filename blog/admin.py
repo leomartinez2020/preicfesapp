85a1793d36335ef4df0d4b43a1adc0122102e540
Ubuntu <ubuntu@ip-172-31-28-216.ec2.internal>
2022-11-27 03:25:02 +0000

    Update admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,23 @@
+from pathlib import Path
+import logging
+#import subprocess
+
 from django.contrib import admin
 
 from blog.models import Post, Etiqueta
 
-admin.site.register(Post)
+logging.basicConfig(filename='adminlogs.log', encoding='utf-8', level=logging.DEBUG)
+
+class PostAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        #logging.info(form.cleaned_data['contenido'])
+        root = Path.home()
+        archivo = Path(root, 'saas/bestpreicfes/blog/templates/blog', 'sandbox.html')
+        with open(archivo, 'a') as html:
+            html.write(form.cleaned_data['contenido'])
+            logging.info('Content copied...')
+        #subprocess.check_call('npx tailwindcss -i ./src/input.css -o ./static/css/output.css', shell=True)
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Etiqueta)
--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.text import slugify
 from django.urls import reverse
 
-from ckeditor.fields import RichTextField
+#from ckeditor.fields import RichTextField
 
 
 class Etiqueta(models.Model):
@@ -19,7 +19,7 @@ class Post(models.Model):
     titulo = models.CharField(max_length=255, unique=True)
     info = models.CharField(max_length=255, blank=True)
     slug = models.SlugField(max_length=255, unique=True, null=True, blank=True)
-    contenido = RichTextField()
+    contenido = models.TextField(null=True, blank=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_modificado = models.DateTimeField(auto_now=True)
     fue_publicado = models.BooleanField(default=False)
@@ -35,3 +35,4 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post', args=[str(self.slug)])
+
